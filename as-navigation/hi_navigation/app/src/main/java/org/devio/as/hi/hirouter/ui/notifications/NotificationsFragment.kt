4f0dd5b77cc00011ce6b95afc83bcf8cafb6b568
zion223 <1025618933@qq.com>
2021-06-08 16:25:34 +0800

    docs: 添加注释

--- as-navigation/hi_navigation/app/src/main/java/org/devio/as/hi/hirouter/ui/notifications/NotificationsFragment.kt
+++ as-navigation/hi_navigation/app/src/main/java/org/devio/as/hi/hirouter/ui/notifications/NotificationsFragment.kt
@@ -10,33 +10,64 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import org.devio.`as`.hi.hirouter.R
-import org.devio.`as`.hi.hirouter.ui.notifications.NotificationsViewModel
 import org.devio.`as`.hi.nav_annotation.Destination
 
 @Destination(pageUrl = "main/tabs/notification")
 class NotificationsFragment : Fragment() {
 
+    private val TAG: String = "NotificationsFragment"
+
     private lateinit var notificationsViewModel: NotificationsViewModel
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         notificationsViewModel =
-            ViewModelProviders.of(this).get(NotificationsViewModel::class.java)
+                ViewModelProviders.of(this).get(NotificationsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_notifications, container, false)
         val textView: TextView = root.findViewById(R.id.text_notifications)
         notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
 
-        Log.e("fragment", "NotificationsFragment,onCreateView")
+        Log.e(TAG, "NotificationsFragment,onCreateView")
         return root
     }
 
     override fun onResume() {
         super.onResume()
-        Log.e("fragment", "NotificationsFragment,onResume")
+        Log.e(TAG, "NotificationsFragment,onResume")
+    }
+
+    override fun onDestroy() {
+        Log.e(TAG, "NotificationsFragment,onDestroy")
+        super.onDestroy()
+    }
+
+    override fun onStart() {
+        Log.e(TAG, "NotificationsFragment,onStart")
+        super.onStart()
+    }
+
+    override fun onPause() {
+        Log.e(TAG, "NotificationsFragment,onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.e(TAG, "NotificationsFragment,onStop")
+        super.onStop()
+    }
+
+    override fun onDestroyView() {
+        Log.e(TAG, "NotificationsFragment,onDestroyView")
+        super.onDestroyView()
+    }
+
+    override fun onDetach() {
+        Log.e(TAG, "NotificationsFragment,onDetach")
+        super.onDetach()
     }
 }
